694903f2a8abebcd2d6207beedc44b5a91252ca2
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-11-03 01:02:11 -0300

    feat(home): init creation of home component, and implements footer component

---
+++ src/app/shared/banner/banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent {
+@Input() imgSrc: string = '';
+@Input() imgAlt: string = '';
+
+}
