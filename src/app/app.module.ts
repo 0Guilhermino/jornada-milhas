e91b4bd58c07ea561f2f57cb887e7299b1f53097
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-11-04 23:44:52 -0300

    feat(login):init implements login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { DropdownUfComponent } from './shared/form-busca/dropdown-uf/dropdown-uf.component';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { SelecaoPassageiroComponent } from './shared/selecao-passageiro/selecao-passageiro.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { SelecaoPassageiroComponent } from './shared/selecao-passageiro/selecao-
     ModalComponent,
     BotaoControleComponent,
     DropdownUfComponent,
-    SelecaoPassageiroComponent
+    SelecaoPassageiroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
