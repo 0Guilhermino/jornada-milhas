5f6d133d798a9a1de4889e4dd7a06a4cdb3a6c6c
Angelo Guilhermino <angeloguilhermino@gmail.com>
2024-11-13 14:28:20 -0300

    feat(token):implements token service

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  salvarToken(token: string){
+    localStorage.setItem(KEY, token);
+  }
+
+  excluirToken(){
+    localStorage.removeItem(KEY);
+  }
+
+  getToken(): string | null{
+    return localStorage.getItem(KEY);
+  }
+
+  possuiToken(): boolean{
+    return !!this.getToken();
+  }
+
+
+}
